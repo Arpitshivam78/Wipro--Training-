15262eb7944181137cccc672cef7307b33b4e6be
Arpit Shivam <arpitshivam17@gmail.com>
2025-04-29 03:02:03 -0700

    This is my second code

---
+++ 29 April Assignment/Assignment 2/First.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {   
+        int age  = 25;
+        double Fees = 2345.67;
+        string name = "jj"; 
+        bool istudent = true; 
+        Console.WriteLine ("hellow world"); 
+        Console.WriteLine ("age" + age); 
+        Console.WriteLine ("Fees" + Fees); 
+        Console.WriteLine ("name" + name); 
+        Console.WriteLine ("isStudent" + istudent);
+    } 
+} 
+
+// Add 
+using System;
+
+public class HelloWorld
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Main()
+    {
+        int num1 = 45;
+        int num2 = 67;
+
+        Console.WriteLine("Addition: " + Add(num1, num2));
+    }
+}
