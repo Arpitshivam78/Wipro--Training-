a27eaf830f67218c2d8044280bc25ad693810ace
Arpit Shivam <arpitshivam17@gmail.com>
2025-04-30 16:30:53 +0530

    Assignment of 30th April

--- 30 April Assignment/Assignment_5.cs
+++ 30 April Assignment/Assignment_5.cs
@@ -1,31 +1,18 @@
 using System;
 
-class Program 
-{ 
-    static void SearchElement(int[] arr, int found) 
-    { 
-        bool isFound = false;
+class Program
+{
+    static void SearchElement(int[] arr, int target)
+    {
+        int index = Array.IndexOf(arr, target);
+        Console.WriteLine(index >= 0
+            ? $"Element {target} found at index {index}"
+            : $"Element {target} not found");
+    }
 
-        for (int i = 0; i < arr.Length; i++) 
-        { 
-            if (arr[i] == found) 
-            { 
-                Console.WriteLine($"Element {found} found at index {i}"); 
-                isFound = true;
-                break; 
-            } 
-        }
-
-        if (!isFound)
-        {
-            Console.WriteLine($"Element {found} not found");
-        }
-    } 
-
-    static void Main() 
-    { 
-        int[] numbers = {3, 5, 6, 7, 8, 9, 10};  
-        int found = 7; 
-        SearchElement(numbers, found); 
-    }  
+    static void Main()
+    {
+        int[] numbers = { 3, 5, 6, 7, 8, 9, 10 };
+        SearchElement(numbers, 7);
+    }
 }
