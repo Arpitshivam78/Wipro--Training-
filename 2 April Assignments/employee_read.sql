76cc69fef9a547059a34f854dcc2206ad761b9af
Arpit Shivam <arpitshivam417@gmail.com>
2025-05-03 13:37:05 +0530

    2 April Assignment

---
+++ 2 April Assignments/employee_read.sql
@@ -0,0 +1,14 @@
+-- Employee Table
+CREATE TABLE EMPLOYEE( 
+EmployeeID INT PRIMARY KEY, 
+FIRSTNAME VARCHAR(50), 
+LASTNAME VARCHAR(50), 
+Department VARCHAR(50), 
+SALARY DECIMAL(10, 2), 
+HireDate DATE 
+
+); 
+
+
+-- read 
+-- Employee Table
