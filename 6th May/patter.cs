9a3688e59ea28ced25b4b9c23607d9eff7e82442
Arpit Shivam <arpitshivam17@gmail.com>
2025-05-06 15:59:34 +0530

    pattern.cs

---
+++ 6th May/patter.cs
@@ -0,0 +1,18 @@
+// print a trangle pattern 
+
+using System; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
