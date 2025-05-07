b9f6f0ec4760a27b7493f3d841b02fa86ecb89ee
Arpit Shivam <arpitshivam417@gmail.com>
2025-05-07 12:15:56 +0530

    Collection.arraylist

--- 7TH MAY/collection.cs
+++ 7TH MAY/collection.cs
@@ -6,7 +6,9 @@ class Program{
           list.Add("Apple"); 
           list.Add(42); 
           list.Add(3.14); 
-
+         
+          list.Insert(1, "Inserted"); 
+          Console.WriteLine("Element is inserted " + list[1]); 
          foreach (var item in list){ 
                 Console.WriteLine(item); 
          }  
