3d512243cdd957abf7e1b0c71c43f58181ae9f0a
Arpit Shivam <arpitshivam417@gmail.com>
2025-05-08 12:03:39 +0530

    Dictionary

--- 8TH MAY/INT_LIST/dictionary.cs
+++ 8TH MAY/INT_LIST/dictionary.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 
 class Program
 { 
-         static void Main()
-         { 
+         static void Main(){ 
                Dictionary <int , string> students = new Dictionary<int, string>(); 
-                
-               students.Add(101, "John"); 
-               students.Add(102, "Jane"); 
-               students.Add(103, "Sam"); 
+                students.Add(101, "John"); 
+                students.Add(102, "Jane"); 
+                students.Add(103, "Sam"); 
 
             Console.WriteLine("StudenT With ID 102: " + students[102]);  
 
-            foreach (var pair in students)
-            { 
-                Console.WriteLine($"ID: {pair.Key}, Name: {pair.Value}"); 
+            foreach (var pain in students){ 
+                Console.WriteLine($"ID:" {pair.Key}, "Name": {pair.Value}); 
             }    
          }             
 }
