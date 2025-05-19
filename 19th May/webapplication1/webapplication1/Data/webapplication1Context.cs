6c01851316a1afbb24ff54e9e350c10627920b27
Arpit Shivam <arpitshivam17@gmail.com>
2025-05-19 16:36:53 +0530

    AArpit - 27383

---
+++ 19th May/webapplication1/webapplication1/Data/webapplication1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webapplication1.Models;
+
+namespace webapplication1.Data
+{
+    public class webapplication1Context : DbContext
+    {
+        public webapplication1Context (DbContextOptions<webapplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webapplication1.Models.Student> Student { get; set; } = default!;
+    }
+}
