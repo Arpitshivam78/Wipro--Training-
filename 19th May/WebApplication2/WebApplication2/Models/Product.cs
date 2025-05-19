e32577e5c8e00f50f7cbd267eaeeed11d77e50a3
Arpit Shivam <arpitshivam17@gmail.com>
2025-05-19 23:32:11 +0530

    Arpit - 27383

---
+++ 19th May/WebApplication2/WebApplication2/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    { 
+        public int ProductID { get; set; } 
+        public string ProductName { get; set; } 
+
+        public decimal Price { get; set; } 
+
+       // public Category Category { get; set; }  
+    }
+
+    /*public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+    } 
+    */  
+
+}
