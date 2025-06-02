38e9037e0e1a0dd0af5d9e3f0ae14f29a84ff272
Arpit Shivam <arpitshivam17@gmail.com>
2025-06-02 17:09:20 +0530

    Arpit - 27383

---
+++ 2nd June Assinment/WebApplication3/WebApplication3/Migrations/20250602113038_SecondCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication3.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FiratName",
+                table: "Employee",
+                newName: "FirstName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Employee",
+                newName: "FiratName");
+        }
+    }
+}
---
+++ 2nd June Assinment/WebApplication3/WebApplication3/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication3.Models
+{
+    public class Employee
+    { 
+        public int Id { get; set; } 
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+
+        public int Salary { get; set; } 
+
+
+    }
+}
