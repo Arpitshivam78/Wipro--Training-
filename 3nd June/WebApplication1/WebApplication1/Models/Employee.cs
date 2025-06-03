73903f15d98b3df02fab007d1c0e601c769f7375
Arpit Shivam <arpitshivam17@gmail.com>
2025-06-03 11:54:29 +0530

    Arpit - 27383

---
+++ 3nd June/WebApplication1/WebApplication1/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    { 
+        public int Id { get; set; }  
+
+        public string FirstName { get; set; } 
+
+        public string LastName { get; set; } 
+
+        public string  City { get; set; } 
+
+        public int Salary { get; set; }
+    }
+}
